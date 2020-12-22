1fea9843a394c05ee996df8af1b3963d7e9d7e6a
bartulem <mimica.bartul@gmail.com>
2020-12-22 17:56:34 +0100

    add quantify_ratemaps v10

--- quantify_ratemaps.py
+++ quantify_ratemaps.py
@@ -8,8 +8,10 @@ Gets (1) tuning peak locations, (2) occupancies, (3) computes inter-session stab
 
 """
 
+import io
 import os
 import sys
+import json
 import numpy as np
 import scipy.io as sio
 from tqdm import tqdm
@@ -58,10 +60,11 @@ def check_curve_exceeds_shuffled(curve1d, shuffled_mean, shuffled_std,  min_acc_
     Returns
     ----------
     truth_value : bool
-        Whether the 1-D tuning curve exceeds shuffled.
+        Whether the 1-D tuning curve exceeds the shuffled distribution.
     ----------
     """
-    if (min_acc_rate is True or curve1d.max() > min_acc_rate) and curve1d.max() > shuffled_mean[np.argmax(curve1d)] + 3*shuffled_std[np.argmax(curve1d)]:
+    if (min_acc_rate is True or curve1d.max() > min_acc_rate) and curve1d.max() \
+            > shuffled_mean[np.argmax(curve1d)] + 3*shuffled_std[np.argmax(curve1d)]:
         peak_position = np.argmax(curve1d)
         if peak_position == 0:
             if (curve1d[peak_position:bin_radius_to_check*3] > shuffled_mean[peak_position:bin_radius_to_check*3] + 3*shuffled_std[peak_position:bin_radius_to_check*3]).all():
@@ -102,7 +105,7 @@ class RatemapCharacteristics:
                         'V2M': {'frank': ['intermediate'], 'johnjohn': ['intermediate'], 'kavorka': ['intermediate']},
                         'V2L': {'frank': ['distal'], 'johnjohn': ['distal'], 'kavorka': ['distal']}}
 
-    def __init__(self, ratemap_mat_dir='', pkl_sessions_dir='', area_filter='M', animal_filter=True, profile_filter=True,
+    def __init__(self, ratemap_mat_dir='', pkl_sessions_dir='', save_dir='', area_filter='M', animal_filter=True, profile_filter=True,
                  session_id_filter='s1', session_type_filter=True, cluster_type_filter=True,
                  cluster_groups_dir='', sp_profiles_csv='', specific_date=None):
         if specific_date is None:
@@ -115,6 +118,7 @@ class RatemapCharacteristics:
                              'kavorka': True}
         self.ratemap_mat_dir = ratemap_mat_dir
         self.pkl_sessions_dir = pkl_sessions_dir
+        self.save_dir = save_dir
         self.area_filter = area_filter
         self.cluster_type_filter = cluster_type_filter
         self.profile_filter = profile_filter
@@ -163,8 +167,9 @@ class RatemapCharacteristics:
 
         Returns
         ----------
-        essential_files (dict)
-            The dictionary of all relevant .mat ratemap files (for max two relevant session types) for further analyses.
+        cluster_dict / essential_files (dict)
+            A dictionary with all the clusters from a certain brain area organized by animal/bank, or
+            a dictionary of all relevant .mat ratemap files (for max two relevant session types) for further analyses.
         ----------
         """
 
@@ -197,7 +202,7 @@ class RatemapCharacteristics:
             return cluster_dict
         else:
             # collect relevant file names in a list
-            essential_files = {'chosen_session_1': [], 'chosen_session_2': []}
+            essential_files = {'chosen_session_1': [], 'chosen_session_2': [], 'unique': []}
             if os.path.exists(self.ratemap_mat_dir):
                 for file_name in tqdm(os.listdir(self.ratemap_mat_dir), desc='Checking all ratemap files'):
                     if (self.animal_filter is True or any(one_animal in file_name for one_animal in self.animal_filter)) \
@@ -212,6 +217,11 @@ class RatemapCharacteristics:
                         if (self.specific_date[animal_id] is True or any(one_date in file_name for one_date in self.specific_date[animal_id])) \
                                 and animal_id in cluster_dict.keys() and bank_id in cluster_dict[animal_id].keys():
                             if cluster_id in cluster_dict[animal_id][bank_id]:
+                                for unique_file in essential_files['unique']:
+                                    if animal_id in unique_file and bank_id in unique_file:
+                                        break
+                                else:
+                                    essential_files['unique'].append(file_name)
                                 if not seek_stability:
                                     essential_files['chosen_session_1'].append(file_name)
                                 else:
@@ -243,7 +253,7 @@ class RatemapCharacteristics:
         use_smoothed_occ (bool)
             Use smoothed occupancies to make ratemaps; defaults to False.
         min_acceptable_occ (float)
-            The minimum acceptable occupancy; defaults to 0.4 (ms).
+            The minimum acceptable occupancy; defaults to 0.4 (s).
         use_smoothed_rm (bool)
             Use smoothed firing rates to make ratemaps; defaults to False.
         ----------
@@ -258,15 +268,17 @@ class RatemapCharacteristics:
         use_smoothed_occ = 6 if 'use_smoothed_occ' in kwargs.keys() and kwargs['use_smoothed_occ'] is True else 2
         min_acceptable_occ = kwargs['min_acceptable_occ'] if 'min_acceptable_occ' in kwargs.keys() and type(kwargs['min_acceptable_occ']) == float else 0.4
         use_smoothed_rm = 3 if 'use_smoothed_rm' in kwargs.keys() and kwargs['use_smoothed_rm'] is True else 1
+        min_acc_rate = kwargs['min_acc_rate'] if 'min_acc_rate' in kwargs.keys() and type(kwargs['min_acc_rate']) == float else True
+        bin_radius_to_check = kwargs['bin_radius_to_check'] if 'bin_radius_to_check' in kwargs.keys() and type(kwargs['bin_radius_to_check']) == int else 1
 
         essential_files = self.file_finder()
 
         # get tining peak locations
         tuning_peak_locations = {}
-        for file in essential_files['chosen_session_1']:
+        for file in tqdm(essential_files['chosen_session_1'], desc='Checking ratemap files for tuning peaks'):
             mat = sio.loadmat(f'{self.ratemap_mat_dir}{os.sep}{file}')
             for key in mat.keys():
-                if 'imec' in key and 'data' in key and 'Back_azimuth-' in key:
+                if 'imec' in key and 'data' in key:
 
                     # find feature ID
                     reduced_key = key[19:]
@@ -278,14 +290,17 @@ class RatemapCharacteristics:
                     valid_rm_range = find_valid_rm_range(rm_occ=mat[key][use_smoothed_occ, :],
                                                          min_acceptable_occ=min_acceptable_occ)
 
+                    # conduct the checking
                     valid_rm = mat[key][use_smoothed_rm, :].take(indices=valid_rm_range)
                     if check_curve_exceeds_shuffled(curve1d=valid_rm,
                                                     shuffled_mean=mat[key][4, :].take(indices=valid_rm_range),
                                                     shuffled_std=mat[key][5, :].take(indices=valid_rm_range),
-                                                    min_acc_rate=True,
-                                                    bin_radius_to_check=1):
+                                                    min_acc_rate=min_acc_rate,
+                                                    bin_radius_to_check=bin_radius_to_check):
                         tuning_peak_locations[feature_id].append(mat[key][0, :].take(indices=valid_rm_range)[np.argmax(valid_rm)])
 
-        print(tuning_peak_locations)
+        # save results to file
+        with io.open(f'{self.save_dir}{os.sep}tuning_peak_locations_{self.area_filter}.json', 'w', encoding='utf-8') as to_save_file:
+            to_save_file.write(json.dumps(tuning_peak_locations, ensure_ascii=False, indent=4))
 
 
